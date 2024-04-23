6f8b43606d99fc893f286f35d7ed845bcc46f9b5
tuantotti <tuantotti14032001@gmail.com>
2024-04-23 07:52:07 +0700

    refactor: add check expire promotion pipelines.py

--- crawler/crawler/pipelines.py
+++ crawler/crawler/pipelines.py
@@ -1,6 +1,10 @@
 import pymongo
 from crawler.utils import CleanText, check_spider_pipeline
+from crawler.items import PromotionDetailItem
 from itemadapter import ItemAdapter
+import re
+from typing import List
+from datetime import datetime
 
 
 class CleanDocumentPipeline:
@@ -10,6 +14,49 @@ class CleanDocumentPipeline:
     @classmethod
     def from_crawler(cls, crawler):
         return cls(clean_text=CleanText())
+    
+    def check_expire_item(self, item: PromotionDetailItem) -> bool:
+        """check whether a promotion item is expire or not expire
+
+        Args:
+            item (PromotionDetailItem): a promotion item
+
+        Returns:
+            bool: True if a promotion is expire otherwise False
+        """
+        date_pattern = r"[0-9]+/[0-9]+/[0-9]+"
+        extract_date = re.findall(date_pattern, item['date_range'])
+
+        return self.is_expire_date(extract_date)
+    
+    def is_expire_date(self, extract_date: List) -> bool:
+        """Check a date or a range of date is expire or not expire compared to current date
+
+        Args:
+            extract_date (List): list of date ([from_date, end_date] or [from_date] or [])
+
+        Returns:
+            bool: True if date is expire, otherwise return False
+        """
+        from_date, end_date = '', ''
+        is_expire = False
+        if len(extract_date) == 1:
+            from_date = extract_date[0]
+
+        if len(extract_date) == 2:
+            from_date = extract_date[0]
+            end_date = extract_date[1]
+
+        today = datetime.today()
+        if end_date:
+            end_date_obj = datetime.strptime(end_date, '%d/%m/%Y')
+            is_expire = from_date < today < end_date_obj
+        elif from_date:
+            is_expire = from_date < today
+        else:
+            is_expire = False
+
+        return is_expire
 
     @check_spider_pipeline
     def process_item(self, item, spider):
